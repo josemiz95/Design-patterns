5c1511b9cb87b2e642aa18cb0e10b3e69c55b5b9
Jose Miguel Ruiz Lopez <jose.ruiz@novafori.com>
2022-08-03 16:03:30 +0200

    Composite

---
+++ DesignPatterns/Structural/Composite/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo.DrawGraphcObjects();
+        }
+    }
+
+    internal static class Demo
+    {
+        public static void DrawGraphcObjects()
+        {
+            var drawing = new GraphicObject { Name = "Drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle { Color = "Blue" });
+            group.Children.Add(new Square { Color = "Blue" });
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
+        }
+    }
+}
