78294ce661a9ef5b3e6e4e3cfe123b0ac35397a6
Jose Miguel Ruiz Lopez <jose.ruiz@novafori.com>
2022-08-11 13:15:54 +0200

    Facade

---
+++ DesignPatterns/Structural/Facade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Facade.systems;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This can be injected
+            Subsystem1 subsystem1 = new Subsystem1();
+            Subsystem2 subsystem2 = new Subsystem2();
+            systems.Facade facade = new systems.Facade(subsystem1, subsystem2);
+
+
+            Demo.RunOperations(facade);
+            Demo.RunCalculate(facade);
+        }
+    }
+
+    class Demo
+    {
+        public static void RunOperations(systems.Facade facade)
+        {
+            Console.Write(facade.Operation());
+        }
+        public static void RunCalculate(systems.Facade facade)
+        {
+            Console.Write(facade.Calculate());
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Facade/systems/SubSystems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.systems
+{
+    public class Subsystem1
+    {
+        public string operation1()
+        {
+            return "Subsystem1: Ready!\n";
+        }
+
+        public string operationN()
+        {
+            return "Subsystem1: Go!\n";
+        }
+    }
+
+    public class Subsystem2
+    {
+        public string operation1()
+        {
+            return "Subsystem2: Get ready!\n";
+        }
+
+        public string operationZ()
+        {
+            return "Subsystem2: Fire!\n";
+        }
+    }
+
+    public class Subsystem3
+    {
+        public string operation()
+        {
+            return "Subsystem3: Doing all calc!\n";
+        }
+    }
+}
