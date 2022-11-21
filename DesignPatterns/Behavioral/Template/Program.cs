7920a389b9893d8bc4eb2e93e5354efab1c9169b
Jose Miguel Ruiz Lopez <jose.ruiz@novafori.com>
2022-11-21 16:12:48 +0100

    Template

---
+++ DesignPatterns/Behavioral/Template/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Template.GameTemplate;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo.PlayChess();
+        }
+    }
+
+    public static class Demo
+    {
+        public static void PlayChess()
+        {
+            var chess = new Chess();
+            chess.Run();
+        }
+    }
+}
